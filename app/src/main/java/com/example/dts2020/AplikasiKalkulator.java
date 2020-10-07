94a8af591c358931f5731576d9361793f92ccd27
Adahuys <shedict@gmail.com>
2020-10-07 19:29:34 +0700

    kalkulator dan main menu

--- app/src/main/java/com/example/dts2020/AplikasiKalkulator.java
+++ app/src/main/java/com/example/dts2020/AplikasiKalkulator.java
@@ -3,12 +3,99 @@ package com.example.dts2020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AplikasiKalkulator extends AppCompatActivity {
+
+    float angkaA;
+    float angkaB;
+    float angkaHasil;
+    String hasil;
+
+    Button plus, minus, kali, bagi, bersihkan;
+    EditText pertama, kedua;
+    TextView txtHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aplikasi_kalkulator);
+
+        plus = (Button) findViewById(R.id.plus);
+        minus = (Button) findViewById(R.id.minus);
+        kali = (Button) findViewById(R.id.kali);
+        bagi = (Button) findViewById(R.id.bagi);
+        bersihkan = (Button) findViewById(R.id.btnClear);
+
+        pertama = (EditText) findViewById(R.id.numA);
+        kedua = (EditText) findViewById(R.id.numB);
+
+        txtHasil = (TextView) findViewById(R.id.txtHasil);
+
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                angkaA = Float.parseFloat(String.valueOf(pertama.getText()));
+                angkaB = Float.parseFloat(String.valueOf(kedua.getText()));
+
+                angkaHasil = angkaA + angkaB;
+
+                hasil = String.valueOf(angkaHasil);
+
+                txtHasil.setText(hasil);
+            }
+        });
+
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                angkaA = Float.parseFloat(String.valueOf(pertama.getText()));
+                angkaB = Float.parseFloat(String.valueOf(kedua.getText()));
+
+                angkaHasil = angkaA - angkaB;
+
+                hasil = String.valueOf(angkaHasil);
+
+                txtHasil.setText(hasil);
+            }
+        });
+        kali.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                angkaA = Float.parseFloat(String.valueOf(pertama.getText()));
+                angkaB = Float.parseFloat(String.valueOf(kedua.getText()));
+
+                angkaHasil = angkaA * angkaB;
+
+                hasil = String.valueOf(angkaHasil);
+
+                txtHasil.setText(hasil);
+            }
+        });
+        bagi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                angkaA = Float.parseFloat(String.valueOf(pertama.getText()));
+                angkaB = Float.parseFloat(String.valueOf(kedua.getText()));
+
+                angkaHasil = angkaA / angkaB;
+
+                hasil = String.valueOf(angkaHasil);
+
+                txtHasil.setText(hasil);
+            }
+        });
+
+        bersihkan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pertama.setText("");
+                kedua.setText("");
+                txtHasil.setText("");
+            }
+        });
     }
 }
--- app/src/main/java/com/example/dts2020/MainActivity.java
+++ app/src/main/java/com/example/dts2020/MainActivity.java
@@ -2,13 +2,53 @@ package com.example.dts2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] menu = new String[]{"Input Nama","Aplikasi Kalkulator","Aplikasi ListView"};
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listMenu);
+
+        ArrayAdapter arrayAdapter =new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                menu);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+                String item = (String) parent.getAdapter().getItem(i);
+
+                Intent pindah = null;
+
+                if(i==0){
+                    pindah =new Intent(MainActivity.this,AplikasiNama.class);
+                    startActivity(pindah);
+                } else if(i==1){
+                    pindah =new Intent(MainActivity.this,AplikasiKalkulator.class);
+                    startActivity(pindah);
+                } else {
+                    pindah =new Intent(MainActivity.this,AplikasiListView.class);
+                    startActivity(pindah);
+                }
+            }
+        });
     }
 }
