e2c0cc050db06b1fe16d7cdebadfbcc1f6e959a3
zuniar.hidaya <zuniar.hidaya@gmail.com>
2020-10-07 15:19:54 +0700

    first commit

---
+++ app/src/main/java/com/example/dts2020/AplikasiListView.java
@@ -0,0 +1,14 @@
+package com.example.dts2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AplikasiListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aplikasi_list_view);
+    }
+}
---
+++ app/src/main/java/com/example/dts2020/AplikasiNama.java
@@ -0,0 +1,14 @@
+package com.example.dts2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AplikasiNama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aplikasi_nama);
+    }
+}
