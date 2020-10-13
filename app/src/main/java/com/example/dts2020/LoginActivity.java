df63c3e7950d2325aaa87fea3bbf7d6afe2bbf7d
zuniar.hidaya <zuniar.hidaya@gmail.com>
2020-10-13 21:28:36 +0700

    Validasi Login

--- app/src/main/java/com/example/dts2020/LoginActivity.java
+++ app/src/main/java/com/example/dts2020/LoginActivity.java
@@ -69,7 +69,7 @@ public class LoginActivity extends AppCompatActivity {
 
             if (datauser[1].equals(password.getText().toString())) {
                 simpanFileLogin();
-                Intent intent = new Intent(this, MainActivity.class);
+                Intent intent = new Intent(this, ProfileActivity.class);
                 startActivity(intent);
                 finish();
             } else {
